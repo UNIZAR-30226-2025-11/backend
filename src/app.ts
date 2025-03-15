a5a302d9674306cfefd58a292d0648b0b1e068b0
Davidjm123 <davidjmzom1@gmail.com>
2025-03-15 15:44:17 +0100

    Added socket debugging .env

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import { Server } from "socket.io";
 import { authRouter } from "./auth/routes.js";
 import { usersRouter } from "./users/routes.js";
 import { setupSocket } from "./socketSetup.js";
-import { FRONTEND_URL } from "./config.js";
+import { FRONTEND_URL, SOCKET_AUT } from "./config.js";
 import { protectSocket } from "./auth/middleware.js";
 
 export const app = express();
@@ -42,8 +42,10 @@ app.use(authRouter);
 app.use(usersRouter);
 
 //app.use(handleErrors); // This runs if an exception is not handled earlier
+console.log("SOCKET_AUT", SOCKET_AUT);
 
-io.use(protectSocket);
+if(SOCKET_AUT)
+  io.use(protectSocket);
 
 // Set up the sockets
 setupSocket(io);
--- src/config.ts
+++ src/config.ts
@@ -3,20 +3,25 @@ import assert from "node:assert";
 
 config();
 
-if (!process.env.PORT) console.log("Using default PORT");
-if (!process.env.PGHOST) console.log("Using default PGHOST");
-if (!process.env.PGPORT) console.log("Using default PGHOST");
+if (!process.env.PORT) console.log(`Using default PORT: 8000`);
+if (!process.env.PGHOST) console.log(`Using default PGHOST: localhost`);
+if (!process.env.PGPORT) console.log("Using default PGPORT: 5432 ");
 if (!process.env.FRONTEND_URL)
   console.log(
     "Using wildcard CORS origin. REQUESTS WITH COOKIES WILL BE THROWN OUT",
   );
 assert(process.env.JWT_SECRET, "No JWT_SECRET provided");
 
-export const PORT = process.env.PORT || 8000;
+export const SOCKET_AUT: boolean = process.env.SOCKAUT?.toLowerCase() === "true"
 
-export const PGHOST = process.env.PGHOST || "localhost";
-export const PGPORT = Number(process.env.PGPORT) || 5432;
+if (!SOCKET_AUT)
+  console.log("Not using socket authentication via JWT Cookies. RECOMMENDED ONLY FOR SOCKER DEBUGGING PURPOSES!")
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || "*";
+export const PORT: string = process.env.PORT || "8000";
 
-export const JWT_SECRET = process.env.JWT_SECRET;
+export const PGHOST: string = process.env.PGHOST || "localhost";
+export const PGPORT: number = Number(process.env.PGPORT) || 5432;
+
+export const FRONTEND_URL: string = process.env.FRONTEND_URL || "*";
+
+export const JWT_SECRET: string = process.env.JWT_SECRET;
