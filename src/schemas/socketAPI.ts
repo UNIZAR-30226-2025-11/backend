c477472167ff0fcabb4424f3024a30257eb61e6c
Davidjm123 <davidjmzom1@gmail.com>
2025-03-27 20:48:51 +0100

    Added some comprobations

--- src/schemas/socketAPI.ts
+++ src/schemas/socketAPI.ts
@@ -1,9 +1,12 @@
 import { z } from "zod";
 
+const CardTypeSchema = z.enum(["Attack", "Skip", "Shuffle", "SeeFuture", "Nope", "Favor", "Deactivate", "RainbowCat", "TacoCat", "HairyPotatoCat", "Cattermelon", "BeardCat"]);
+const LobbyIdSchema = z.string().length(9, "Lobby ID must be 9 characters long");
+
 // Card schema
 export const CardJSONSchema = z.object({
     id: z.number(),
-    type: z.string(),
+    type: CardTypeSchema,
 });
 
 // FrontendGamePlayedCardsJSON
@@ -11,7 +14,7 @@ export const FrontendGamePlayedCardsJSONSchema = z.object({
     error: z.boolean(),
     errorMsg: z.string(),
     playedCards: z.array(CardJSONSchema),
-    lobbyId: z.string(),
+    lobbyId: LobbyIdSchema,
 });
 
 // FrontendWinnerResponseJSON
@@ -20,7 +23,7 @@ export const FrontendWinnerResponseJSONSchema = z.object({
     errorMsg: z.string(),
     winnerUsername: z.string(),
     coinsEarned: z.number(),
-    lobbyId: z.string(),
+    lobbyId: LobbyIdSchema,
 });
 
 // FrontendGameSelectPlayerResponseJSON
@@ -28,7 +31,7 @@ export const FrontendGameSelectPlayerResponseJSONSchema = z.object({
     error: z.boolean(),
     errorMsg: z.string(),
     playerUsername: z.string(),
-    lobbyId: z.string(),
+    lobbyId: LobbyIdSchema,
 });
 
 // FrontendGameSelectCardResponseJSON
@@ -36,15 +39,15 @@ export const FrontendGameSelectCardResponseJSONSchema = z.object({
     error: z.boolean(),
     errorMsg: z.string(),
     card: CardJSONSchema,
-    lobbyId: z.string(),
+    lobbyId: LobbyIdSchema,
 });
 
 // FrontendGameSelectCardTypeResponseJSON
 export const FrontendGameSelectCardTypeResponseJSONSchema = z.object({
     error: z.boolean(),
     errorMsg: z.string(),
-    cardType: z.string(),
-    lobbyId: z.string(),
+    cardType: CardTypeSchema,
+    lobbyId: LobbyIdSchema,
 });
 
 // FrontendGameSelectNopeResponseJSON
@@ -52,34 +55,34 @@ export const FrontendGameSelectNopeResponseJSONSchema = z.object({
     error: z.boolean(),
     errorMsg: z.string(),
     useNope: z.boolean(),
-    lobbyId: z.string(),
+    lobbyId: LobbyIdSchema,
 });
 
 // FrontendCreateLobbyJSON
 export const FrontendCreateLobbyJSONSchema = z.object({
     error: z.boolean(),
     errorMsg: z.string(),
-    maxPlayers: z.number(),
+    maxPlayers: z.number().min(2, "Minimum number of players is 2").max(4, "Maximum number of players is 4"),
 });
 
 // FrontendJoinLobbyJSON
 export const FrontendJoinLobbyJSONSchema = z.object({
     error: z.boolean(),
     errorMsg: z.string(),
-    lobbyId: z.string(),
+    lobbyId: LobbyIdSchema,
 });
 
 // FrontendStartLobbyJSON
 export const FrontendStartLobbyJSONSchema = z.object({
     error: z.boolean(),
     errorMsg: z.string(),
-    lobbyId: z.string(),
+    lobbyId: LobbyIdSchema,
 });
 
 // FrontendPostMsgJSON
 export const FrontendPostMsgJSONSchema = z.object({
     error: z.boolean(),
     errorMsg: z.string(),
-    msg: z.string(),
-    lobbyId: z.string(),
+    msg: z.string().min(1, "Message must be at least 1 character long"),
+    lobbyId: LobbyIdSchema,
 });
